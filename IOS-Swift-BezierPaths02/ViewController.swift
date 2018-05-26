0b01f11d81e28f3672e48c71b841322927d97ae3
Pooya Hatami <pooya@naghmehs-macbook-pro-2.local>
2018-05-26 03:55:27 -0700

    Done :)

--- IOS-Swift-BezierPaths02/ViewController.swift
+++ IOS-Swift-BezierPaths02/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let width : CGFloat = 150.0
+        let height : CGFloat = 150.0
+        
+        let demoView = DemoView(frame: CGRect(x: self.view.frame.size.width/2 - width/2 , y: self.view.frame.size.height/2 - height/2 , width: width, height: height))
+        
+        self.view.addSubview(demoView)
     }
+    
 
 
 }
